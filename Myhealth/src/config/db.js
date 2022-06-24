33cf7cf2a63d5d48428edb9ac14a11c3842aa292
Lofuno Kgotlagomang <lofunok@gmail.com>
2022-06-25 00:04:29 +0200

    App functionality

---
+++ Myhealth/src/config/db.js
@@ -0,0 +1,12 @@
+import Firebase from "firebase";
+let config = {
+  apiKey: "AIzaSyA060qd9WWud3l2o3EtAPfRX-zEwTYqDaQ",
+  authDomain: "myhealth-bfff0.firebaseapp.com",
+  projectId: "myhealth-bfff0",
+  storageBucket: "myhealth-bfff0.appspot.com",
+  messagingSenderId: "346169846585",
+  appId: "1:346169846585:web:bb49ea8c7973ab11bf96e8",
+  measurementId: "G-CEWNBV6W8X",
+};
+let app = Firebase.initializeApp(config);
+export const db = app.database();

24dee6a97deb43c072ab5d4215ad9a1cccc8a66e
Lofuno Kgotlagomang <lofunok@gmail.com>
2022-06-27 16:58:46 +0200

    Fix page sources

--- Myhealth/routes/UserNavigation.js
+++ Myhealth/routes/UserNavigation.js
@@ -1,27 +1,27 @@
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import { NavigationContainer } from "@react-navigation/native";
-import UserLoginScreen from "../screens/userScreens/UserLoginScreen";
-import UserHome from "../screens/userScreens/UserHome";
-import SignUpScreen from "../screens/userScreens/SignUpScreen";
+import UserLoginScreen from ".src/screens/userScreens/UserLoginScreen";
+import UserHome from ".src/screens/userScreens/UserHome";
+import SignUpScreen from ".src/screens/userScreens/SignUpScreen";
 
 const UserStack = createNativeStackNavigator();
 export default function UserNavigation() {
   return (
     <NavigationContainer>
-      <UserStack.Navigator initialRouteName='Login'>
+      <UserStack.Navigator initialRouteName="Login">
         <UserStack.Screen
           options={{ headerShown: false }}
-          name='Login'
+          name="Login"
           component={UserLoginScreen}
         />
         <UserStack.Screen
           options={{ headerShown: false }}
-          name='Home'
+          name="Home"
           component={UserHome}
         />
         <UserStack.Screen
           options={{ headerShown: false }}
-          name='SignUp'
+          name="SignUp"
           component={SignUpScreen}
         />
       </UserStack.Navigator>
